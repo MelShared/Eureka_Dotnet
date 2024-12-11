36828d9186cc15f0db3e3a4d3c28c52d1e93f584
Mathias Alejandro Guevara Cruz <140579994+Chaps0218@users.noreply.github.com>
2024-12-10 22:32:39 -0500

    AAAAAAAAAAA

---
+++ RESTFUL_DOTNET/01.Servidor/WS_EUREKA_RESTFUL_DOTNET/WS_EUREKA_RESTFUL_DOTNET/Controllers/EurekaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WS_EUREKA_RESTFUL_DOTNET.Controllers
+{
+    public class EurekaController : Controller
+    {
+        // GET: Eureka
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RESTFUL_DOTNET/01.Servidor/WS_EUREKA_RESTFUL_DOTNET/WS_EUREKA_RESTFUL_DOTNET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WS_EUREKA_RESTFUL_DOTNET.MvcApplication" Language="C#" %>
